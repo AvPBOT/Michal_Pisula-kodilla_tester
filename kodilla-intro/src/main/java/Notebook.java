56c463c063a0ad467d1fd2edc2eaf4eec9b75e3c
Michal Pisula <mwpisula@gmail.com>
2022-10-16 15:37:24 +0200

    New module

--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -12,9 +12,9 @@ public class Notebook {
     public void checkPrice() {
         if (this.price < 600) {
             System.out.println("This notebook is very cheap.");
-        } else if (this.price > 600 && this.price <= 1000) {
+        }else if (this.price > 600 && this.price <= 1000) {
             System.out.println("The price is good.");
-        } else {
+        }else{
             System.out.println("This notebook is expensive.");
         }
     }
@@ -22,20 +22,22 @@ public class Notebook {
     public void checkWeight() {
         if (this.weight < 700) {
             System.out.println("This device is light.");
-        } else if (this.weight > 700 && this.weight <= 1800) {
+        }else if (this.weight > 700 && this.weight <= 1800) {
             System.out.println("This device is not very heavy");
-        } else {
+        }else{
             System.out.println("This device is very heavy.");
         }
     }
 
     public void checkYearPrice() {
-        if ((this.price <= 600) && (this.year < 2018)) {
+        if ((this.price <= 600) && (this.year < 2015)) {
             System.out.println("It's a lost artifact of the ancient world.");
-        } else if ((this.price > 600 && this.price <= 1000) && (this.year >= 2018)) {
+        } else if ((this.price > 600 && this.price <= 1000) && (this.year >= 2015)) {
             System.out.println("It's a sensible choice.");
-        } else if ((this.price > 1000) && (this.year < 2018)) {
+        } else if ((this.price > 1000) && (this.year < 2015)) {
             System.out.println("It's a scam.");
+        }else{
+            System.out.println("It's an anomaly!");
         }
 
     }
