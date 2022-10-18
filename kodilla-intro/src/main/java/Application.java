fd1a50c70e6f939acfafbf1c27863383e1c32168
Michal Pisula <mwpisula@gmail.com>
2022-10-18 22:28:14 +0200

    Module 2

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,32 @@
+public class Application {
+    private String name;
+    private double age;
+    private double height;
+
+    public Application(String name, double age, double height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void parametersCheck() {
+        if(this.name == null) {
+            return;
+        }
+        if(this.age > 30 && this.height > 160) {
+            System.out.println(this.name + " is older than 30 and taller than 160 cm.");
+        }
+        else {
+            System.out.println(this.name + " is younger than 30 or shorter than 160 cm.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Application adam = new Application("Adam", 40.5, 178);
+        System.out.println("Name: " + adam.name);
+        System.out.println("Age: " + adam.age);
+        System.out.println("Height: " + adam.height);
+        adam.parametersCheck();
+
+    }
+}
---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,16 @@
+
+public class Book {
+    private String author;
+    private String title;
+
+    public Book(String author, String title){
+    }
+
+    public static Book of(String author, String title) {
+        return new Book(author, title);
+    }
+
+    public static void main(String[] args) {
+        Book theGalaxy = Book.of("Isaac Asimov", "The Galaxy");
+    }
+}
