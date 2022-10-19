8396570f6956556e8a06b638f16b536c90a2cb33
Michal Pisula <mwpisula@gmail.com>
2022-10-19 16:03:14 +0200

    Module 2

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -2,20 +2,14 @@
 public class Book {
     private String author;
     private String title;
-
     public Book(String author, String title){
     }
-
     public static Book of(String author, String title) {
-
         return new Book(author, title);
     }
-
     public static void main(String[] args) {
-
         Book theGalaxy = Book.of("Isaac Asimov", "The Galaxy");
         System.out.println(theGalaxy.author);
         System.out.println(theGalaxy.title);
-
     }
 }
