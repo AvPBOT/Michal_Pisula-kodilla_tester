b479db7c1e2305cbad823335710642161097557d
Michał <114862082+AvPBOT@users.noreply.github.com>
2022-10-26 12:55:42 +0200

    TA-3.2 (#8)
    Dziedziczenie i klasy abstrakcyjne

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/Dog.java
@@ -0,0 +1,10 @@
+package com.kodilla.abstracts;
+public class Dog extends Animal{
+    public Dog() {
+        super(4);
+    }
+    @Override
+    public void giveVoice() {
+        System.out.println("Hau hau");
+    }
+}
