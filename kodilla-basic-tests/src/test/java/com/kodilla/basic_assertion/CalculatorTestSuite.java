f209f38a03db798f4db927e7d18d4de6f4fbc2ad
Michal Pisula <mwpisula@gmail.com>
2022-10-26 12:27:48 +0200

    Zadanie: rozbudowa testów kalkulatora

---
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -0,0 +1,49 @@
+package com.kodilla.basic_assertion;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult, 0.01);
+    }
+
+    @Test
+    public void testSub() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subResult = calculator.sub(a,b);
+        assertEquals(-3, subResult, 0.01);
+    }
+
+    @Test
+    public void testSqNeg() {
+        Calculator calculator = new Calculator();
+        int a = -5;
+        int sqResultNeg = calculator.sq(a);
+        assertEquals(25, sqResultNeg, 0.01);
+    }
+
+    @Test
+    public void testSqZero() {
+        Calculator calculator = new Calculator();
+        int a = 0;
+        int sqResultZ = calculator.sq(a);
+        assertEquals(0, sqResultZ, 0.01);
+    }
+
+    @Test
+    public void testSquarePos() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int sqResultPos = calculator.sq(a);
+        assertEquals(25, sqResultPos, 0.01);
+    }
+}
