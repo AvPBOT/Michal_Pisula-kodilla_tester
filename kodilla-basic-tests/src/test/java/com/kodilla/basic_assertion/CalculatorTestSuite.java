6ad9677a4c3a63b32ffcc80ced42917f5316f9f2
Michał <114862082+AvPBOT@users.noreply.github.com>
2022-10-30 18:58:16 +0100

    TAX-3.6 (#6)
    TAX-3.6 after review

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,7 +1,6 @@
 package com.kodilla.basic_assertion;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTestSuite {
@@ -13,5 +12,38 @@ public class CalculatorTestSuite {
         int b = 8;
         int sumResult = calculator.sum(a, b);
         assertEquals(13, sumResult);
+    }
+
+    @Test
+    public void testSub() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subResult = calculator.sub(a,b);
+        assertEquals(-3, subResult);
+    }
+
+    @Test
+    public void testSqNeg() {
+        Calculator calculator = new Calculator();
+        int a = -5;
+        int sqResultNeg = calculator.sq(a);
+        assertEquals(25, sqResultNeg);
+    }
+
+    @Test
+    public void testSqZero() {
+        Calculator calculator = new Calculator();
+        int a = 0;
+        int sqResultZ = calculator.sq(a);
+        assertEquals(0, sqResultZ);
+    }
+
+    @Test
+    public void testSquarePos() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int sqResultPos = calculator.sq(a);
+        assertEquals(25, sqResultPos);
     }
 }
