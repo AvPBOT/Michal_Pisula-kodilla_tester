3010a10ce33cda5c4d187a1c1c58b0aa44b77943
Michal Pisula <mwpisula@gmail.com>
2022-11-02 16:36:32 +0100

    TAX-4.3

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
@@ -1,28 +1,25 @@
 package com.kodilla.collections.arrays.homework;
 
+import com.kodilla.collections.interfaces.homework.BMW;
 import com.kodilla.collections.interfaces.homework.Car;
 import com.kodilla.collections.interfaces.homework.Ford;
 import com.kodilla.collections.interfaces.homework.Opel;
-import com.kodilla.collections.interfaces.homework.BMW;
 
 public class CarUtils {
 
     public static void describeCar(Car car) {
         System.out.println("Car type: " + getCarName(car));
-        System.out.println("Car name: " + car.toString());
+        System.out.println("Car name: " + car);
     }
 
     static String getCarName(Car car) {
-        if(car instanceof Ford)
+        if (car instanceof Ford) {
             return "Ford";
-
-        else if(car instanceof Opel)
+        } else if (car instanceof Opel) {
             return "Opel";
-
-        else if(car instanceof BMW)
+        } else if (car instanceof BMW) {
             return "Volkswagen";
-
-        else
-            return "Unknown car name.";
+        }
+        return null;
     }
 }
