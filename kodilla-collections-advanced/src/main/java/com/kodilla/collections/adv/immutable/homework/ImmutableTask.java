8ac8b9400abfb6b967d4d2a137678c9776c754d1
Michal Pisula <mwpisula@gmail.com>
2022-11-08 00:49:17 +0100

    TAX-5.3

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/ImmutableTask.java
@@ -0,0 +1,8 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class ImmutableTask {
+    public static void main(String[] args) {
+        Task task = new Task("ImmutableTask", 10);
+        TaskRecord taskRecord = new TaskRecord("ImmutableTest", 20, 2022);
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskHacked.java
@@ -0,0 +1,11 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+class TaskHacked extends Task {
+    public TaskHacked(String title, int duration) {
+        super(title, duration);
+    }
+
+    public void modifyTitle(String newTitle) {
+        title = newTitle;
+    }
+}
