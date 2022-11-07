b8add4a72abe27ea67dea44169f66f286295f344
Michal Pisula <mwpisula@gmail.com>
2022-11-08 00:27:50 +0100

    TAX-5.2

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -0,0 +1,31 @@
+package com.kodilla.collections.adv.maps.homework;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SchoolDirectory {
+    public static void main(String[] args) {
+        Map<Principal, School> school = new HashMap<>();
+
+        Principal albus = new Principal("Albus", "Dumbledore", "Hogwarts School of Witchcraft and Wizardry");
+        Principal olympe = new Principal("Olympe", "Maxime", "Beauxbatons Academy of Magic");
+        Principal igor = new Principal("Igor", "Karkaroff", "Durmstrang Institute");
+        Principal agilbert = new Principal("Agilbert", "Fontaine", "Ilvermorny School of Witchcraft and Wizardry");
+
+        School hogwartsStudents = new School(Arrays.asList(65, 22, 41));
+        School beauxbatonsStudents = new School(Arrays.asList(49, 53, 21));
+        School durmstrangStudents = new School(Arrays.asList(53, 23, 61));
+        School ilvermornyStudentsStudents = new School(Arrays.asList(29, 42, 44));
+
+        school.put(albus, hogwartsStudents);
+        school.put(olympe, beauxbatonsStudents);
+        school.put(igor, durmstrangStudents);
+        school.put(agilbert, ilvermornyStudentsStudents);
+
+        for(Map.Entry<Principal, School> principalEntry : school.entrySet()) {
+            System.out.println("Number of students in " + principalEntry.getKey().getForename() + " " + principalEntry.getKey().getSurname() + "'s " + principalEntry.getKey().getSchoolName() + ": " +  principalEntry.getValue().getSum());
+        }
+
+    }
+}
