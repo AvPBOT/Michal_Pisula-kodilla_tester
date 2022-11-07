837cc5d465f8d7e9dcf0d80b058ff9298f4c9d0b
Michal Pisula <mwpisula@gmail.com>
2022-11-07 21:51:54 +0100

    TAX-2.6

--- kodilla-intro/src/main/java/ZadanieApplication.java
+++ kodilla-intro/src/main/java/ZadanieApplication.java
@@ -3,18 +3,22 @@ public class ZadanieApplication {
     public static void main(String[] args) {
         Agent adam = new Agent("Adam", 40.5, 178);
         System.out.println(adam);
-        if (adam.isOlderThan() && adam.isTallerThan()) {
-            System.out.println(adam.getName() + " is older than 30 and taller than 160 cm.");
-        } else {
-            System.out.println(adam.getName() + " is younger than 30 or shorter than 160 cm.");
+        if (adam.getName() != null) {
+            if (adam.isOlderThan() && adam.isTallerThan()) {
+                System.out.println(adam.getName() + " is older than 30 and taller than 160 cm.");
+            } else {
+                System.out.println(adam.getName() + " is younger than 30 or shorter than 160 cm.");
+            }
         }
 
         Agent test1 = new Agent("Test1", 27, 188);
         System.out.println(test1);
-        if (test1.isOlderThan() && test1.isTallerThan()) {
-            System.out.println(test1.getName() + " is older than 30 and taller than 160 cm.");
-        } else {
-            System.out.println(test1.getName() + " is younger than 30 or shorter than 160 cm.");
+        if (test1.getName() != null) {
+            if (test1.isOlderThan() && test1.isTallerThan()) {
+                System.out.println(test1.getName() + " is older than 30 and taller than 160 cm.");
+            } else {
+                System.out.println(test1.getName() + " is younger than 30 or shorter than 160 cm.");
+            }
         }
     }
 }
