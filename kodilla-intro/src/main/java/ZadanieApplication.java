75e64b806c73e1336ed5244040b9f0bc2289ab14
Michal Pisula <mwpisula@gmail.com>
2022-11-08 19:32:36 +0100

    TAX-2.6

--- kodilla-intro/src/main/java/ZadanieApplication.java
+++ kodilla-intro/src/main/java/ZadanieApplication.java
@@ -3,7 +3,7 @@ public class ZadanieApplication {
     public static void main(String[] args) {
         Agent adam = new Agent("Adam", 40.5, 178);
         System.out.println(adam);
-        if (adam.hasName() == true) {
+        if (adam.hasName()) {
             if (adam.isOlderThan(30) && adam.isTallerThan(160)) {
                 System.out.println(adam.getName() + " is older than 30 and taller than 160 cm.");
             } else {
@@ -13,7 +13,7 @@ public class ZadanieApplication {
 
         Agent test1 = new Agent("Test1", 27, 188);
         System.out.println(test1);
-        if (test1.hasName() == true) {
+        if (test1.hasName()) {
             if (test1.isOlderThan(30) && test1.isTallerThan(160)) {
                 System.out.println(test1.getName() + " is older than 30 and taller than 160 cm.");
             } else {
