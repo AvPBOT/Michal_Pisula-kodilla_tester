7fc823a89a76ca3d8962612e5f2e13458049b6c0
Michal Pisula <mwpisula@gmail.com>
2022-11-11 18:52:12 +0100

    TAX-6.1

--- kodilla-stream/src/test/java/com/kodilla/stream/UsersManagerTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/UsersManagerTest.java
@@ -2,13 +2,44 @@ package com.kodilla.stream;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UsersManagerTest {
 
     @Test
     void testFilterChemistGroupUsernames() {
+        // when
+        UsersManager.filterChemistGroupUsernames();
+        // then
+        List<String> expectedUsernames = new ArrayList<>();
+            expectedUsernames.add("Walter White");
+            expectedUsernames.add("Gale Boetticher");
+        assertEquals(expectedUsernames, UsersManager.filterChemistGroupUsernames());
+    }
 
+    @Test
+    void testFilterOlderThanUsernames() {
+        // when
+        UsersManager.filterOlderThanUsernames(40);;
+        // then
+        List<String> expectedUsernames = new ArrayList<>();
+        expectedUsernames.add("Walter White");
+        expectedUsernames.add("Gus Firing");
+        expectedUsernames.add("Gale Boetticher");
+        expectedUsernames.add("Mike Ehrmantraut");
+        assertEquals(expectedUsernames, UsersManager.filterOlderThanUsernames(40));
+    }
 
+    @Test
+    void testFilterChemistsWithNumberOfPostHigherThanUsernames() {
+        // when
+        UsersManager.filterChemistsWithNumberOfPostHigherThan(5);;
+        // then
+        List<String> expectedUsernames = new ArrayList<>();
+        expectedUsernames.add("Walter White");
+        assertEquals(expectedUsernames, UsersManager.filterChemistsWithNumberOfPostHigherThan(5));
     }
 }
