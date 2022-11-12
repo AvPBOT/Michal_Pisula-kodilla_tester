9a1e3b144b40b87e1f25a6f57f2e5a032c132588
Michał <114862082+AvPBOT@users.noreply.github.com>
2022-11-12 21:06:20 +0100

    TAX-4.4 (#22)
    TAX-4.4 after review

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -0,0 +1,29 @@
+package com.kodilla.collections.sets.homework;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StampsApplication {
+    public static void main(String[] args) {
+        Set<Stamp> stamps = new HashSet<>();
+        stamps.add(new Stamp("British Guiana 1c Magenta", 0.29, 0.26, false));
+        stamps.add(new Stamp("Czerwony Mauritius", 0.19, 0.22, true));
+        stamps.add(new Stamp("Czerwony Mauritius", 0.19, 0.22, true));
+        stamps.add(new Stamp("Czerwony Mauritius", 0.19, 0.22, true));
+        stamps.add(new Stamp("Inverted Jenny", 2.22, 2.54, true));
+        stamps.add(new Stamp("Odwrotka bokserów", 2, 1, false));
+        stamps.add(new Stamp("Polska nr 1", 3, 2, false));
+
+        for(Stamp stamp: stamps) {
+            System.out.println("Dodano do kolekcji: " + stamp);
+        }
+
+        System.out.println();
+        System.out.println("Rozmiar kolekcji: " + stamps.size());
+        if (stamps.size() == 5) {
+            System.out.println("OK, the size of the collection is correct.");
+        } else {
+            System.out.println("Something is bad!");
+        }
+    }
+}
