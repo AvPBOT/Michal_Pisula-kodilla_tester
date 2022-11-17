a33c2f05cb8d6e993062a00cf42920086a2d4242
Michal Pisula <mwpisula@gmail.com>
2022-11-17 23:07:07 +0100

    TAX-6.3

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -3,47 +3,28 @@ package com.kodilla.stream.homework;
 import com.kodilla.stream.User;
 import com.kodilla.stream.UsersRepository;
 
+import java.util.List;
+
 public class ForumStats {
-
-    static double avgAboveForty = UsersRepository.getUsersList()
-            .stream()
-            .filter(u -> u.getAge() >= 40)
-            .map(User::getNumberOfPost)
-            .mapToDouble(n -> n)
-            .average()
-            .orElse(Double.NaN);
-
-    static double avgBelowForty = UsersRepository.getUsersList()
-            .stream()
-            .filter(u -> u.getAge() < 40)
-            .map(User::getNumberOfPost)
-            .mapToDouble(n -> n)
-            .average()
-            .orElse(Double.NaN);
-
-    static double avgAboveFiftyFive = UsersRepository.getUsersList()
-            .stream()
-            .filter(u -> u.getAge() > 55)
-            .map(User::getNumberOfPost)
-            .mapToDouble(n -> n)
-            .average()
-            .orElse(Double.NaN);
-
-    public static double getAvgAboveForty() {
-        return avgAboveForty;
-    }
-
-    public static double getAvgBelowForty() {
-        return avgBelowForty;
-    }
-
-    public static double getAvgAboveFiftyFive() {
-        return avgAboveFiftyFive;
-    }
-
     public static void main(String[] args) {
-        System.out.println("Average number of posts by users whose age is >= 40: " + getAvgAboveForty());
-        System.out.println("Average number of posts for users whose age is < 40: " + getAvgBelowForty());
-        System.out.println("Average number of posts for users whose age is >55: " + getAvgAboveFiftyFive());
+        List<User> users = UsersRepository.getUsersList();
+
+        double avgAboveForty = users
+                .stream()
+                .filter(u -> u.getAge() >= 40)
+                .map(User::getNumberOfPost)
+                .mapToDouble(n -> n)
+                .average()
+                .orElse(Double.NaN);
+        System.out.println("Average number of posts by users whose age is >= 40: " + avgAboveForty);
+
+        double avgBelowForty = users
+                .stream()
+                .filter(u -> u.getAge() < 40)
+                .map(User::getNumberOfPost)
+                .mapToDouble(n -> n)
+                .average()
+                .orElse(Double.NaN);
+        System.out.println("Average number of posts by users whose age is < 40: " + avgBelowForty);
     }
 }
