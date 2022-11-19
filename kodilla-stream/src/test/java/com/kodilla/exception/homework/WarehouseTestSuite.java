e20ef7b79fe413a4cecfe9ad8a98089f47f0fd00
Michal Pisula <mwpisula@gmail.com>
2022-11-19 13:26:27 +0100

    TAX-6.5

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class WarehouseTestSuite {
+
+    @Test
+    public void testThrowException() {
+        Throwable exception = assertThrows(
+                OrderDoesntExistException.class, () -> {
+                    Warehouse.addOrder(new Order("1"));
+                    Warehouse.addOrder(new Order("2"));
+                    Warehouse.addOrder(new Order("3"));
+                    Warehouse.getOrder("4");
+                }
+        );
+        assertEquals("Order number 4 not found!", exception.getMessage());
+    }
+}
