54a1b2a9d253b83c9ca8cc603532d4273100a1fa
Michal Pisula <mwpisula@gmail.com>
2022-11-22 20:48:50 +0100

    TAX-7.3

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -3,72 +3,16 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PersonTestSuite {
     @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingVerySeverelyUnderweight")
-    public void returnVerySeverelyUnderweight(double heightInMeters, double weightInKilogram) {
-        assertEquals("Very severely underweight", new Person(heightInMeters, weightInKilogram).getBMI());
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingSeverelyUnderweight")
-    public void returnSeverelyUnderweight(double heightInMeters, double weightInKilogram) {
-        assertEquals("Severely underweight", new Person(heightInMeters, weightInKilogram).getBMI());
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingUnderweight")
-    public void returnUnderweight(double heightInMeters, double weightInKilogram) {
-        assertEquals("Underweight", new Person(heightInMeters, weightInKilogram).getBMI());
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingNormalWeight")
-    public void returnNormalWeight(double heightInMeters, double weightInKilogram) {
-        assertEquals("Normal (healthy weight)", new Person(heightInMeters, weightInKilogram).getBMI());
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingOverweight")
-    public void returnNormalOverweight(double heightInMeters, double weightInKilogram) {
-        assertEquals("Overweight", new Person(heightInMeters, weightInKilogram).getBMI());
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingObeseClassI")
-    public void returnObeseClassI(double heightInMeters, double weightInKilogram) {
-        assertEquals("Obese Class I (Moderately obese)", new Person(heightInMeters, weightInKilogram).getBMI());
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingObeseClassII")
-    public void returnObeseClassII(double heightInMeters, double weightInKilogram) {
-        assertEquals("Obese Class II (Severely obese)", new Person(heightInMeters, weightInKilogram).getBMI());
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingObeseClassIII")
-    public void returnObeseClassIII(double heightInMeters, double weightInKilogram) {
-        assertEquals("Obese Class III (Very severely obese)", new Person(heightInMeters, weightInKilogram).getBMI());
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingObeseClassIV")
-    public void returnObeseClassIV(double heightInMeters, double weightInKilogram) {
-        assertEquals("Obese Class IV (Morbidly Obese)", new Person(heightInMeters, weightInKilogram).getBMI());
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingObeseClassV")
-    public void returnObeseClassV(double heightInMeters, double weightInKilogram) {
-        assertEquals("Obese Class V (Super Obese)", new Person(heightInMeters, weightInKilogram).getBMI());
-    }
-
-    @ParameterizedTest
-    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingObeseClassVI")
-    public void returnObeseClassVI(double heightInMeters, double weightInKilogram) {
-        assertEquals("Obese Class VI (Hyper Obese)", new Person(heightInMeters, weightInKilogram).getBMI());
+    @MethodSource(value = "com.kodilla.parametrized_tests.homework.TestSources#provideDataForTestingBMI")
+    void testCalculateBMIWithProvidedDataExternalSource(Person person) {
+        Stream<String> expected = Stream.of("Very severely underweight", "Severely underweight", "Underweight", "Normal (healthy weight)", "Overweight", "Obese Class I (Moderately obese)", "Obese Class II (Severely obese)", "Obese Class III (Very severely obese)", "Obese Class IV (Morbidly Obese)", "Obese Class V (Super Obese)", "Obese Class VI (Hyper Obese)");
+        boolean match = expected.anyMatch(u -> u.equals(person.getBMI()));
+        assertTrue(match);
     }
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/TestSources.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/TestSources.java
@@ -3,102 +3,19 @@ import org.junit.jupiter.params.provider.Arguments;
 import java.util.stream.Stream;
 
 public class TestSources {
-    static Stream<Arguments> provideDataForTestingVerySeverelyUnderweight() {
+    static Stream<Arguments> provideDataForTestingBMI() {
         return Stream.of(
-                Arguments.of(1.95, 56),
-                Arguments.of(1.90, 40),
-                Arguments.of(1.70, 42),
-                Arguments.of(1.85, 50)
-        );
-    }
-
-    static Stream<Arguments> provideDataForTestingSeverelyUnderweight() {
-        return Stream.of(
-                Arguments.of(1.95, 60),
-                Arguments.of(1.80, 50),
-                Arguments.of(1.70, 46),
-                Arguments.of(1.85, 54)
-        );
-    }
-
-    static Stream<Arguments> provideDataForTestingUnderweight() {
-        return Stream.of(
-                Arguments.of(1.95, 68),
-                Arguments.of(1.65, 48),
-                Arguments.of(1.75, 54),
-                Arguments.of(1.50, 40)
-        );
-    }
-
-    static Stream<Arguments> provideDataForTestingNormalWeight() {
-        return Stream.of(
-                Arguments.of(1.95, 78),
-                Arguments.of(1.50, 52),
-                Arguments.of(1.65, 60),
-                Arguments.of(1.80, 72)
-        );
-    }
-
-    static Stream<Arguments> provideDataForTestingOverweight() {
-        return Stream.of(
-                Arguments.of(1.95, 112),
-                Arguments.of(1.75, 90),
-                Arguments.of(1.40, 58),
-                Arguments.of(1.60, 76)
-        );
-    }
-
-    static Stream<Arguments> provideDataForTestingObeseClassI() {
-        return Stream.of(
-                Arguments.of(1.95, 130),
-                Arguments.of(1.65, 94),
-                Arguments.of(1.75, 106),
-                Arguments.of(1.55, 84)
-        );
-    }
-
-    static Stream<Arguments> provideDataForTestingObeseClassII() {
-        return Stream.of(
-                Arguments.of(1.85, 136),
-                Arguments.of(1.65, 108),
-                Arguments.of(1.50, 88),
-                Arguments.of(1.35, 72)
-        );
-    }
-
-    static Stream<Arguments> provideDataForTestingObeseClassIII() {
-        return Stream.of(
-                Arguments.of(1.75, 136),
-                Arguments.of(1.55, 106),
-                Arguments.of(1.45, 94),
-                Arguments.of(1.35, 82)
-        );
-    }
-
-    static Stream<Arguments> provideDataForTestingObeseClassIV() {
-        return Stream.of(
-                Arguments.of(1.65, 134),
-                Arguments.of(1.55, 118),
-                Arguments.of(1.35, 90),
-                Arguments.of(1.45, 104)
-        );
-    }
-
-    static Stream<Arguments> provideDataForTestingObeseClassV() {
-        return Stream.of(
-                Arguments.of(1.50, 134),
-                Arguments.of(1.45, 126),
-                Arguments.of(1.40, 116),
-                Arguments.of(1.35, 108)
-        );
-    }
-
-    static Stream<Arguments> provideDataForTestingObeseClassVI() {
-        return Stream.of(
-                Arguments.of(1.95, 300),
-                Arguments.of(1.80, 210),
-                Arguments.of(1.55, 220),
-                Arguments.of(1.45, 190)
+                Arguments.of(new Person(1.95, 56)),
+                Arguments.of(new Person(1.85, 54)),
+                Arguments.of(new Person(1.50, 40)),
+                Arguments.of(new Person(1.80, 72)),
+                Arguments.of(new Person(1.60, 76)),
+                Arguments.of(new Person(1.55, 84)),
+                Arguments.of(new Person(1.35, 72)),
+                Arguments.of(new Person(1.35, 82)),
+                Arguments.of(new Person(1.45, 104)),
+                Arguments.of(new Person(1.35, 108)),
+                Arguments.of(new Person(1.45, 190))
         );
     }
 }
