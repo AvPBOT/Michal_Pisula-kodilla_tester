dca0cf5a927453e1afee9a2da4ca14d690a30cb7
Michal Pisula <mwpisula@gmail.com>
2022-11-27 14:10:36 +0100

    TAX-6.3

--- kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
@@ -31,7 +31,7 @@ class ForumStatsTest {
         // given
         testee = new ForumStats(List.of(new User("Walter White", 50, 7, "Chemists"), new User("Jessie Pinkman", 25, 4648, "Sales"), new User("Tuco Salamanca", 34, 116, "Manager"), new User("Gus Firing", 49, 0, "Board"), new User("Gale Boetticher", 44, 2, "Chemists"), new User("Mike Ehrmantraut", 57, 0, "Security")));
         // then
-        assertEquals(2382.0, testee.getAvgBelowForty());
+        assertEquals(2382.0, testee.getAvgBelowForty(), 0.01);
     }
 
     @Test
