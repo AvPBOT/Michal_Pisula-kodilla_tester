998a7fca9575a92e214bc1eef7d64dd8808c01f7
Michal Pisula <mwpisula@gmail.com>
2022-12-03 20:10:09 +0100

    TAX-6.3

--- kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/homework/ForumStatsTest.java
@@ -10,52 +10,46 @@ import static java.lang.Double.NaN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ForumStatsTest {
-    private ForumStats testee;
+    private ForumStats testeeUsersDepository;
+    private ForumStats testeeEmptyList;
+    private ForumStats testeeNoUsersAboveForty;
+    private ForumStats testeeNoUsersBelowForty;
 
     @BeforeEach
     public void initUsersList() {
-        testee = new ForumStats(List.of(new User("Walter White", 50, 7, "Chemists"), new User("Jessie Pinkman", 25, 4648, "Sales"), new User("Tuco Salamanca", 34, 116, "Manager"), new User("Gus Firing", 49, 0, "Board"), new User("Gale Boetticher", 44, 2, "Chemists"), new User("Mike Ehrmantraut", 57, 0, "Security")));
+        testeeUsersDepository = new ForumStats(List.of(new User("Walter White", 50, 7, "Chemists"), new User("Jessie Pinkman", 25, 4648, "Sales"), new User("Tuco Salamanca", 34, 116, "Manager"), new User("Gus Firing", 49, 0, "Board"), new User("Gale Boetticher", 44, 2, "Chemists"), new User("Mike Ehrmantraut", 57, 0, "Security")));
+        testeeNoUsersAboveForty = new ForumStats(List.of(new User("Walter White", 1, 0, "Chemists"), new User("Jessie Pinkman", 25, 0, "Sales"), new User("Tuco Salamanca", 34, 0, "Manager"), new User("Gus Firing", 2, 0, "Board"), new User("Gale Boetticher", 3, 0, "Chemists"), new User("Mike Ehrmantraut", 4, 0, "Security")));
+        testeeNoUsersBelowForty = new ForumStats(List.of(new User("Walter White", 50, 7, "Chemists"), new User("Jessie Pinkman", 40, 4648, "Sales"), new User("Tuco Salamanca", 41, 116, "Manager"), new User("Gus Firing", 49, 0, "Board"), new User("Gale Boetticher", 44, 2, "Chemists"), new User("Mike Ehrmantraut", 57, 0, "Security")));
+        testeeEmptyList  = new ForumStats(new ArrayList<>());
     }
 
     @Test
      void testAvgAboveFortyForUsersRepository() {
-        assertEquals(2.25, testee.avgAboveForty(), 0.01);
+        assertEquals(2.25, testeeUsersDepository.avgAboveForty(), 0.01);
     }
 
     @Test
     void testAvgAboveFortyButNoUsersAboveForty() {
-        // given
-        testee = new ForumStats(List.of(new User("Walter White", 1, 0, "Chemists"), new User("Jessie Pinkman", 25, 0, "Sales"), new User("Tuco Salamanca", 34, 0, "Manager"), new User("Gus Firing", 2, 0, "Board"), new User("Gale Boetticher", 3, 0, "Chemists"), new User("Mike Ehrmantraut", 4, 0, "Security")));
-        // then
-        assertEquals(NaN, testee.avgAboveForty());
+        assertEquals(NaN, testeeNoUsersAboveForty.avgAboveForty());
     }
 
     @Test
     void testAvgAboveFortyButEmptyList() {
-        // given
-        testee = new ForumStats(new ArrayList<>());
-        // then
-        assertEquals(NaN, testee.avgAboveForty());
+        assertEquals(NaN, testeeEmptyList.avgAboveForty());
     }
 
     @Test
     void testAvgBelowFortyForUsersRepository() {
-        assertEquals(2382.0, testee.avgBelowForty(), 0.01);
+        assertEquals(2382.0, testeeUsersDepository.avgBelowForty(), 0.01);
     }
 
     @Test
     void testAvgBelowFortyButNoUsersBelowForty() {
-        // given
-        testee = new ForumStats(List.of(new User("Walter White", 50, 7, "Chemists"), new User("Jessie Pinkman", 40, 4648, "Sales"), new User("Tuco Salamanca", 41, 116, "Manager"), new User("Gus Firing", 49, 0, "Board"), new User("Gale Boetticher", 44, 2, "Chemists"), new User("Mike Ehrmantraut", 57, 0, "Security")));
-        // then
-        assertEquals(NaN, testee.avgBelowForty());
+        assertEquals(NaN, testeeNoUsersBelowForty.avgBelowForty());
     }
 
     @Test
     void testAvgBelowFortyButEmptyList() {
-        // given
-        testee = new ForumStats(new ArrayList<>());
-        // then
-        assertEquals(NaN, testee.avgBelowForty());
+        assertEquals(NaN, testeeEmptyList.avgBelowForty());
     }
 }
