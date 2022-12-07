8e14c6fe13160857c5ce209cc29a527585e747df
Michal Pisula <mwpisula@gmail.com>
2022-12-07 16:06:07 +0100

    TAX-7.2

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -1,7 +1,39 @@
 package com.kodilla.parametrized_tests.homework;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class GamblingMachineTestSuite {
+    GamblingMachine gamblingMachine = new GamblingMachine();
 
+    /*
+    @ParameterizedTest
+    @CsvFileSource(resources = "/validNumbers.csv")
+    public void testHowManyWins(String numbers) throws InvalidNumbersException {
+
+        });
+    }
+     */
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/invalidNumbers.csv")
+    public void testHowManyWins_withException(String numbers) {
+        String[] myStringNumbers = numbers.split(",");
+        Set<Integer> expectedSet = new HashSet<>();
+        for (int i = 0; i < myStringNumbers.length; i++) {
+            Integer a = Integer.parseInt(myStringNumbers[i]);
+            expectedSet.add(a);
+        }
+        Exception exception = assertThrows(InvalidNumbersException.class, () -> {
+            gamblingMachine.howManyWins(expectedSet);
+        }
+        );
+        assertEquals("Wrong numbers provided", exception.getMessage());
+    }
 }
