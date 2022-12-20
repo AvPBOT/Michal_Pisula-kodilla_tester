5aa9a21028798d251374b0da291aa30759d06543
Michal Pisula <mwpisula@gmail.com>
2022-12-20 15:59:54 +0100

    TAX-9.5

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
@@ -1,22 +1,21 @@
 package com.kodilla.spring.basic.spring_scopes.homework;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 @SpringBootTest
 class ClockTestSuite {
 
     @Test
-    public void shouldShowADifferentTime() {
+    public void testIfTimeInBeansDiffers() {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         Clock clock1 = context.getBean(Clock.class);
         Clock clock2 = context.getBean(Clock.class);
         Clock clock3 = context.getBean(Clock.class);
-        Assertions.assertNotEquals(clock1.getTime(), clock2.getTime());
-        Assertions.assertEquals(clock2.getTime(), clock3.getTime()); // ????
-        Assertions.assertNotEquals(clock1.getTime(), clock3.getTime());
+        assertFalse(clock1.getTime().isBefore(clock2.getTime()) && clock2.getTime().isBefore(clock3.getTime()) && clock1.getTime().isBefore(clock3.getTime()));
     }
 }
