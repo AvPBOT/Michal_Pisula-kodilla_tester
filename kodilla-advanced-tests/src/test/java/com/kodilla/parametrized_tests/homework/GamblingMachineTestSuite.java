96f19d9d9b2b8a65ae1e817633682a20f37dd355
Michał <114862082+AvPBOT@users.noreply.github.com>
2022-12-26 15:40:06 +0100

    TAX-7.2 (#63)
    TAX-7.2 after review

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -0,0 +1,40 @@
+package com.kodilla.parametrized_tests.homework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class GamblingMachineTestSuite {
+    GamblingMachine gamblingMachine = new GamblingMachine();
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/validNumbers.csv")
+    public void testHowManyWins(String numbers) throws InvalidNumbersException {
+        String[] stringNumbers = numbers.split(",");
+        Set<String> numbersSet = new HashSet<>(Arrays.asList(stringNumbers));
+        Set<Integer> myNumbers = numbersSet
+                .stream()
+                .map(u -> Integer.parseInt(u))
+                .collect(Collectors.toSet());
+        assertTrue(gamblingMachine.howManyWins(myNumbers) <= 6 && gamblingMachine.howManyWins(myNumbers) >= 0);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/invalidNumbers.csv", delimiterString = ";")
+    public void testHowManyWins_withException(String numbers) {
+        String[] stringNumbers = numbers.split(",");
+        Set<String> numbersSet = new HashSet<>(Arrays.asList(stringNumbers));
+        Set<Integer> myNumbers = numbersSet
+                .stream()
+                .map(u -> Integer.parseInt(u))
+                .collect(Collectors.toSet());
+        Assertions.assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(myNumbers));
+    }
+}
