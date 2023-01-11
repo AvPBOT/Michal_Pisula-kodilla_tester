59049b349595ee55e13ee62a341ccb5cb97b6acb
Michal Pisula <mwpisula@gmail.com>
2023-01-11 16:19:03 +0100

    TAX-13.4

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -5,7 +5,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) throws InterruptedException {
@@ -13,12 +17,26 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl");
 
-        WebElement category = driver.findElement(By.xpath("//*[@data-role=\"filters-dropdown-toggle\"]"));
+        WebDriverWait wait1 = new WebDriverWait(driver, 10);
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@data-role=\"accept-consent\"]")));
+
+        WebElement cookiesButton = driver.findElement(By.xpath("//*[@data-role=\"accept-consent\"]"));
+        cookiesButton.click();
+
+        WebElement category = driver.findElement(By.cssSelector("select[data-role='filters-dropdown-toggle']"));
         Select selectCategory = new Select(category);
         selectCategory.selectByIndex(3);
-        WebElement searchBox = driver.findElement(By.xpath("//*[@data-role=\"search-input\"]/input"));
+        WebElement searchBox = driver.findElement(By.cssSelector("input[data-role='search-input']"));
         searchBox.sendKeys("Mavic mini");
-        WebElement searchButton = driver.findElement(By.xpath("//*[@data-role=\"search-button\"]"));
+        WebElement searchButton = driver.findElement(By.cssSelector("button[data-role='search-button']"));
         searchButton.click();
+
+        WebDriverWait wait2 = new WebDriverWait(driver, 10);
+        wait2.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section>article")));
+
+        List<WebElement> listOfArticles = driver.findElements(By.cssSelector("section>article"));
+        for (WebElement article : listOfArticles) {
+            System.out.println(article.getText());
+        }
     }
 }
