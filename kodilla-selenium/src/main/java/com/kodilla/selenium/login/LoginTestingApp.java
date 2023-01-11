534d8e4fbec417e30b59442673f266ed96c9c9f4
Michal Pisula <mwpisula@gmail.com>
2023-01-11 17:37:19 +0100

    TA-13.6

--- kodilla-selenium/src/main/java/com/kodilla/selenium/login/LoginTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/login/LoginTestingApp.java
@@ -9,7 +9,7 @@ public class LoginTestingApp {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://kodilla.com/pl/test/store");
+        driver.get("https://kodilla.com/pl/test/login");
 
         WebElement inputField = driver.findElement(By.xpath("//html/body/section/form/div[1]/input"));
         inputField.sendKeys("testuser@gmail.com");
