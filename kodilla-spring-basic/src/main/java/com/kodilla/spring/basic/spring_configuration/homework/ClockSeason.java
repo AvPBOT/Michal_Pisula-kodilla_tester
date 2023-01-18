b5bf1b0aea5037a460f6f888a73948a83f572a71
Michał <114862082+AvPBOT@users.noreply.github.com>
2023-01-18 14:28:02 +0100

    TAX-9.4 (#71)
    TAX-9.4 after review

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/ClockSeason.java
@@ -0,0 +1,34 @@
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ClockSeason {
+    private Season season;
+
+    public Season getSeason() {
+        switch (LocalDate.now().getMonthValue()) {
+            case 12:
+            case 1:
+            case 2:
+                return Season.WINTER;
+            case 3:
+            case 4:
+            case 5:
+                return Season.SPRING;
+            case 6:
+            case 7:
+            case 8:
+                return Season.SUMMER;
+            case 9:
+            case 10:
+            case 11:
+                return Season.AUTUMN;
+        }
+        return season;
+    }
+
+    public LocalTime getTime() {
+        return LocalTime.now();
+    }
+}
