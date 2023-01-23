9b6bc92df190e27711d69afe4cd5ab4a5853f130
Michal Pisula <mwpisula@gmail.com>
2023-01-23 20:38:43 +0100

    TAX-13.6

--- kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/homework/KodillaStorePom.java
+++ kodilla-login-selenium/src/main/java/com/kodilla/selenium/pom/homework/KodillaStorePom.java
@@ -1,20 +1,17 @@
 package com.kodilla.selenium.pom.homework;
 
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class KodillaStorePom {
 
     @FindBy(css = "input[name ='search']")
     WebElement searchField;
-
-    @FindAll({@FindBy(css = "#elements-wrapper")})
-    WebElement result;
-
     WebDriver driver;
 
     public KodillaStorePom(WebDriver driver){
@@ -22,10 +19,11 @@ public class KodillaStorePom {
         PageFactory.initElements(driver,this);
     }
 
-    public Dimension searchPhrase(String search) {
+    public int searchPhrase(String search) {
         searchField.clear();
         searchField.sendKeys(search);
-        Dimension size = result.getSize();
+        List<WebElement> results = driver.findElements(By.cssSelector("#elements-wrapper > div"));
+        int size = results.size();
         return size;
     }
 }
--- kodilla-login-selenium/src/test/java/com/kodilla/selenium/pom/homework/KodillaStorePomTest.java
+++ kodilla-login-selenium/src/test/java/com/kodilla/selenium/pom/homework/KodillaStorePomTest.java
@@ -2,7 +2,6 @@ package com.kodilla.selenium.pom.homework;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -23,37 +22,49 @@ public class KodillaStorePomTest {
 
     @Test
     public void testStorePage_NoteBookSearch() {
-        Dimension noteBook = storePom.searchPhrase("NoteBook");
+        int noteBook = storePom.searchPhrase("NoteBook");
+        int noteBookCaseInsensitive = storePom.searchPhrase("NOTEBOOK");
         assertEquals(2, noteBook);
+        assertEquals(2, noteBookCaseInsensitive);
     }
 
     @Test
     public void testStorePage_SchoolSearch() {
-        Dimension school = storePom.searchPhrase("School");
+        int school = storePom.searchPhrase("School");
+        int schoolCaseInsensitive = storePom.searchPhrase("SCHOOL");
         assertEquals(1, school);
+        assertEquals(1, schoolCaseInsensitive);
     }
 
     @Test
     public void testStorePage_BrandSearch() {
-        Dimension brand = storePom.searchPhrase("NoteBook");
-        assertEquals(2, brand);
+        int brand = storePom.searchPhrase("Brand");
+        int brandCaseInsensitive = storePom.searchPhrase("BRAND");
+        assertEquals(1, brand);
+        assertEquals(1, brandCaseInsensitive);
     }
 
     @Test
     public void testStorePage_BusinessSearch() {
-        Dimension business = storePom.searchPhrase("Business");
+        int business = storePom.searchPhrase("Business");
+        int businessCaseInsensitive = storePom.searchPhrase("BUSINESS");
         assertEquals(0, business);
+        assertEquals(0, businessCaseInsensitive);
     }
 
     @Test
     public void testStorePage_GamingSearch() {
-        Dimension gaming = storePom.searchPhrase("Business");
+        int gaming = storePom.searchPhrase("Gaming");
+        int gamingCaseInsensitive = storePom.searchPhrase("GAMING");
         assertEquals(1, gaming);
+        assertEquals(1, gamingCaseInsensitive);
     }
 
     @Test
     public void testStorePage_PowerfulSearch() {
-        Dimension powerful = storePom.searchPhrase("Business");
+        int powerful = storePom.searchPhrase("Powerful");
+        int powerfulCaseInsensitive = storePom.searchPhrase("POWERFUL");
         assertEquals(0, powerful);
+        assertEquals(0, powerfulCaseInsensitive);
     }
 }
