1e6ee574f082273441bf611edfdba32e8b9e832d
Michal Pisula <mwpisula@gmail.com>
2023-01-29 23:15:09 +0100

    TA-15.5

--- kodilla-code-coverage/src/main/java/com/kodilla/hibernate/task/TaskFinancialDetails.java
+++ kodilla-code-coverage/src/main/java/com/kodilla/hibernate/task/TaskFinancialDetails.java
@@ -1,11 +1,18 @@
 package com.kodilla.hibernate.task;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "TASKS_FINANCIALS_DETAILS")
 public class TaskFinancialDetails {
     private int id;
     private BigDecimal price;
     private boolean paid;
+
+    public TaskFinancialDetails() {
+    }
 
     public TaskFinancialDetails(int id, BigDecimal price, boolean paid) {
         this.id = id;
@@ -13,24 +20,32 @@ public class TaskFinancialDetails {
         this.paid = paid;
     }
 
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "TASKS_FINANCIALS_ID", unique = true)
     public int getId() {
         return id;
+    }
+
+    @NotNull
+    @Column(name = "PRICE")
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    @NotNull
+    @Column(name = "PAID")
+    public boolean getPaid() {
+        return paid;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
-
     public void setPrice(BigDecimal price) {
         this.price = price;
-    }
-
-    public boolean isPaid() {
-        return paid;
     }
 
     public void setPaid(boolean paid) {
