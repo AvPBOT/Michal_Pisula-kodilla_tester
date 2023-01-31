0eab2fb9ff8920e307cb72f44c4921aa835775da
Michal Pisula <mwpisula@gmail.com>
2023-01-31 22:41:58 +0100

    TAX-15.6

--- kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/maps/complex/GradesTest.java
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/maps/complex/GradesTest.java
@@ -3,17 +3,29 @@ package com.kodilla.collections.adv.maps.complex;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class GradesTest {
-    private final Grades marcoGrades = new Grades(Arrays.asList(5.0, 4.0, 4.5, 3.0, 2.0));
+    private final Grades testeeMarcoGrades = new Grades(Arrays.asList(5.0, 4.0, 4.5, 3.0, 2.0));
+
+    private final Grades testeeEmptyGradesList = new Grades(new ArrayList<>());
 
     @Test
     void testGetAverage() {
         //When
-        double marcoAverageGrade = marcoGrades.getAverage();
+        double marcoAverageGrade = testeeMarcoGrades.getAverage();
 
         //Then
         Assertions.assertEquals(3.7, marcoAverageGrade);
+    }
+
+    @Test
+    void testAverageOfEmptyGradesList() {
+        //When
+        double emptyGradesList = testeeEmptyGradesList.getAverage();
+
+        //Then
+        Assertions.assertEquals(Double.NaN, emptyGradesList);
     }
 }
